9050068eb78a63b391ce6c77a9e2191fcdf1ab29
Alecu Cristian <alecucristianionut@gmail.com>
2020-04-22 16:49:18 +0300

    a few courses in

--- helloworld.php
+++ helloworld.php
@@ -9,14 +9,77 @@ $number *= 4;
 
 $a = "number";
 
-echo '\nMy age is ' . $$a;
+echo "\nMy age is " . $$a;
 
 // Arrays
 
 $array = array('name', 'email', 'address');
 print_r($array);
+echo $array[0];
 
-echo $number;
+$array2[] = "salad";
+$array2[] = "bowl";
+print_r($array2);
+
+$array3 = array(
+    'name' => 'John',
+    'age' => 35,
+    'email' => 'email@email.com',
+);
+
+print_r($array3);
+
+// If, else, switch
+$number= 1;
+$number2 = 1;
+$number3 = 3;
+if($number === $number2 || $number === $number3) {
+    echo 'true';
+} else {
+    echo 'false';
+}
+
+switch($number) {
+    case $number2:
+        echo 'equal';
+        break;
+    case 1: 
+        echo 'True';
+        break;
+    case 2:
+        echo 'False';
+        break;
+    default:
+        echo 'no idea';
+        break;
+
+
+}
+
+// For foreach
+echo "<br>";
+for($i=1;$i<=10;$i++){
+    echo "$i<br>";
+
+}
+$array = array("name", "email", "address");
+foreach($array as $key=>$value) {
+    echo "Key $key: $value</br>";
+
+}
+
+// While loops
+echo "<br>";
+$i = 0;
+
+$array = array('name', 'email', 'adress');
+while($array[$i]) {
+    echo "$array[$i] </br>";
+    $i++;
+
+}
+
+// echo $number;
 
 
 

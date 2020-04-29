225827ce3187b2e841fbd1fdd4508a6737671d48
Alecu Cristian <alecucristianionut@gmail.com>
2020-04-29 10:56:14 +0300

    added functions.php

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+// Syntax
+function echo_stuff() {
+    echo 'Hey boss!';
+} 
+
+echo_stuff();
+
+// Function arguments
+function my_name ( $name ) {
+    echo $name;
+
+}
+
+echo '<br>';
+my_name('John');
+my_name( 'Jane' );
+my_name( 'Jeff' );
+
+// Default arguments
+function full_name( $firstname = 'John', $lastname = 'Morris' ) {
+    echo $firstname . $lastname;
+
+}
+
+full_name();
+echo '<br>';
+
+// Return values
+function multiply( $number, $multiplier ) {
+    return $number * $multiplier;
+
+}
+
+
+echo multiply(3, 43);
+echo '<br>';
+echo multiply(41561561, 46546456456);
+
+
+?>

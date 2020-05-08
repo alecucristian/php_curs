dffff482c6c952e28a3da7a866a543a483859b02
Alecu Cristian <alecucristianionut@gmail.com>
2020-05-08 17:20:12 +0300

    added db_class.php and fprm_to_db.php

--- db_class.php
+++ db_class.php
@@ -40,10 +40,74 @@ if (!class_exists('DB' ) ) {
             list( $fields, $placeholders, $values ) = $this->prep_query($data);
 
             // Pretend $format onto $values
+            $values = (array) $values;
             array_unshift($values, $format);
 
             // Preapare our query for binding
             $stmt = $db->prepare("INSERT INTO {$table} ({fields}) VALUES ({$placeholders})");
+
+            // Dinamically bind values
+            call_user_func_array(array( $stmt, 'bind_param'), $this->ref_values($values));
+
+            // Execute the query
+            $stmt->execute();
+
+            // Check for successful insertion
+            if ( $stmt->affected_rows ) {
+                return true;
+            }
+
+            return false;
+
+
+        }
+
+        public function update($table, $data, $format, $where, $where_format) {
+            // Check for $table or $data not set
+            if ( empty( $table ) || empty( $data ) ) {
+                return false;
+            }
+
+            // Connect to the database
+            $db = $this->connect();
+
+            // Cast $data and $format to arrays
+            $data = (array) $data;
+            $format = (array) $format;
+            
+            // Build format string
+            $format = implode('', $format);
+            $format = str_replace('%', '', $format);
+            $where_format = implode('', $where_format);
+            $where_format = str_replace('%', '', $where_format);
+            $format .= $where_format;
+            
+            list( $fields, $placeholders, $values ) = $this->prep_query($data, 'update');
+            
+            // Format where clause
+            $where_clause = '';
+            $where_values = '';
+            $count = 0;
+            
+            foreach ( $where as $field => $value ) {
+                if ( $count > 0) {
+                    $where_clause .= ' AND ';
+                }
+                
+                $where_clause .= $field . '=?';
+                $where_values[] = $value;
+                
+                $count++;
+            }
+            
+            // Prepend $format onto $values
+            $values = (array) $values;
+            $where_values = (array) $where_values;
+            array_unshift($values, $format);
+            $values = array_merge($values, $where_values);
+
+            // Preapare our query for binding
+            $stmt = $db->prepare("UPDATE {$table} SET {$placeholders} WHERE {$where_clause}");
 
             // Dinamically bind values
             call_user_func_array( array( $stmt, 'bind_param' ), $this->ref_values($values));
@@ -58,7 +122,37 @@ if (!class_exists('DB' ) ) {
 
             return false;
 
+        }
 
+        public function get_results($query) {
+            return $this->query($query);
+        } 
+
+        public function get_row($query) {
+            $results = $this->query($query);
+
+            return $results[0];
+        }
+
+        public function delete($table, $id) {
+            // Connect to the database
+            $db = $this->connect();
+
+            // Prepare our query for binding
+            $stmt = $db->prepare("DELETE FROM {$table} WHERE ID = ?");
+            
+            // Dynamically bind values
+            $stmt->bind_param('d', $id);
+
+            // Execute the query
+            $stmt->execute();
+
+            // Check for succesful insertion
+            if ( $stmt->affected_rows) {
+                return true;
+            }
+
+            return false;
         }
 
         private function prep_query($data, $type='insert') {
@@ -99,9 +193,9 @@ if (!class_exists('DB' ) ) {
 
 $db = new DB;
 
-print_r($db->insert('objects', array('post_title'=>'Abstraction Test', 'post_content' => 'Abstranction test content'), array('%s', '%s') ) );
-// print_r($db->update('objects', array('post_title'=>'Abstraction Test Update', 'post_content' => 'Abstraction test update content'), array('%s', '%s') ) );
-// print_r($db->get_results("SELECT * FROM objects" ) );
+// print_r($db->insert('objects', array('post_title'=>'Abstraction Test', 'post_content' => 'Abstranction test content'), array('%s', '%s') ) );
+// print_r($db->update('objects', array('post_title'=>'Abstraction Test Update', 'post_content' => 'Abstraction test update content'), array('%s', '%s'), array('ID'->1), array('%d')  ) );
+print_r($db->get_results("SELECT * FROM objects" ) );
 // print_r($db->get_row("SELECT * FROM objects" ) );
 // print_r($db->delete('objects', 9 ) );
 
